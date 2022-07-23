17ad002bfae7685b1a8dc471f519044ec32885e2
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-07-23 19:44:51 +0100

    ✨ Setting up new socket server with express

--- server.js
+++ server.js
@@ -2,15 +2,41 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-
+const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
   res.json({
-    message: "Hello from the server"
+    message: 'Hello from Express server'
   });
 });
 
+io.on('connection', (socket) => {
+
+    // if a user connects
+    console.log('====================================');
+        console.log('A user connected by socket id: ', socket.id);
+    console.log('====================================');
+
+    // Handle the disconnect event
+    socket.on('disconnect', () => {
+        console.log('====================================');
+        console.log('A user with Id '+socket.id+' has disconnected');
+        console.log('====================================');
+    });
+
+    // Handle the message event
+    socket.on('message', (msg) => {
+        console.log('====================================');
+        console.log('Message from user: ', msg);
+        console.log('====================================');
+    });
+
+    socket.on('message', (msg) => {
+        socket.emit('message', msg);
+    });
+
+});
 
 server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+    console.log('Express and Socket Server are listening on port 3000');
 });
