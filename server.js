9e3fa816f7e1d44a2742e6b33165ecd661758283
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-07-23 22:36:25 +0100

    🐛 Allow all cors origins

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server,{cors: {origin: "*"}});
 const UserController = new (require('./userController'))();
 
 
